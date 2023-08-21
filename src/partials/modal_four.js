17f24d3f6500725b65c0d3686e1e1e89cbd5cc19
DianiMor <133818078+DianiMor@users.noreply.github.com>
2023-08-21 14:32:14 -0500

    js s4

--- src/partials/modal_four.js
+++ src/partials/modal_four.js
@@ -1,4 +1,13 @@
-<script
-  src="https://code.jquery.com/jquery-3.7.0.js"
-  integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM="
-  crossorigin="anonymous"></script>
+const openModal = document.querySelector('.openModal');
+const modal = document.querySelector('.myModal');
+const closeModal = document.querySelector('.closeModal');
+
+openModal.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal.classList.add('modal--show');
+});
+
+closeModal.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal.classList.remove('modal--show');
+});
